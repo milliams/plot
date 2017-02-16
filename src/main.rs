f28373a467c8fc4c07bcb59287f0243f0e33d803
Matt Williams <matt@milliams.com>
2017-02-16 13:13:51 +0000

    Add first pass at average function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use std::io::{self, BufRead};
 
 fn main() {
+    average();
+}
+
+fn average() {
     let stdin = io::stdin();
+    let mut total = 0.0;
+    let mut length = 0;
     for line in stdin.lock().lines() {
-        match line {
-            Ok(line) => println!("{}", line),
+        let line_text = match line {
+            Ok(line) => line,
             Err(err) => panic!("IO error: {}", err),
-        }
+        };
+        length += 1;
+        total += line_text.parse::<f64>().unwrap();
     }
+    println!("{}", total/length as f64);
 }
