791e4c54421b440e1b093a28cb423602fb9a27fc
Matt Williams <matt@milliams.com>
2017-02-23 23:06:38 +0000

    Add distribute test and rustfmt

--- src/main.rs
+++ src/main.rs
@@ -42,10 +42,32 @@ fn get_single_column() -> Vec<f64> {
     return data;
 }
 
+/// Given a list of ticks to display,
+/// the total scale of the axis
+/// and the number of lines to work with,
+/// produce the label for each line of the axis
 fn distribute_ticks_frequency(ticks: Vec<u32>, max: u32, lines: u32) -> Vec<String> {
-    let m: HashMap<_, _> = ticks.iter().map(|&tick| (((tick as f64 / max as f64) * lines as f64) as u32, tick)).collect();
-    let p = (0..lines).map(|line| if m.contains_key(&line) {m[&line].to_string()} else {"".to_string()});
+    let m: HashMap<_, _> = ticks.iter()
+        .map(|&tick| (((tick as f64 / max as f64) * lines as f64) as u32, tick))
+        .collect();
+    let p = (0..lines).map(|line| if m.contains_key(&line) {
+        m[&line].to_string()
+    } else {
+        "".to_string()
+    });
     Vec::from_iter(p)
+}
+
+#[test]
+fn test_distribute_ticks() {
+    assert_eq!(distribute_ticks_frequency(vec![0, 1, 2, 3, 4, 5], 6, 7),
+               ["0", "1", "2", "3", "4", "5", ""]);
+    assert_eq!(distribute_ticks_frequency(vec![0, 1, 2, 3, 4, 5], 6, 8),
+               ["0", "1", "2", "", "3", "4", "5", ""]);
+    assert_eq!(distribute_ticks_frequency(vec![0, 1, 2, 3, 4, 5], 6, 9),
+               ["0", "1", "", "2", "3", "", "4", "5", ""]);
+    assert_eq!(distribute_ticks_frequency(vec![0, 1, 2, 3, 4, 5], 6, 10),
+               ["0", "1", "", "2", "", "3", "4", "", "5", ""]);
 }
 
 fn hist() {
@@ -62,10 +84,12 @@ fn hist() {
 
     let longest_y_label_width = ticks.iter().map(|n| n.to_string().len()).max().unwrap();
 
-    let axis_strings = distribute_ticks_frequency(ticks.clone(), *h.bin_counts.iter().max().unwrap(), plot_height);
+    let axis_strings = distribute_ticks_frequency(ticks.clone(),
+                                                  *h.bin_counts.iter().max().unwrap(),
+                                                  plot_height);
 
     for line in 0..plot_height {
-        let axis_label = axis_strings[(plot_height-line-1) as usize].to_string();
+        let axis_label = axis_strings[(plot_height - line - 1) as usize].to_string();
         let mut cols = String::new();
         for &bin_count in h.bin_counts.iter() {
             // between 0..1 how full the bin is compared to largest
