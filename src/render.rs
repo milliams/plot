3783cc580986a1a7ca44e1808bb7a75b8832e9f7
Matt Williams <matt@milliams.com>
2017-02-26 15:00:27 +0000

    Finish adding ticks to x-axis

--- src/render.rs
+++ src/render.rs
@@ -2,6 +2,7 @@
 
 use std::collections::HashMap;
 use std::iter::FromIterator;
+use std::fmt;
 
 use histogram;
 
@@ -249,6 +250,80 @@ pub fn tick_offset_map(ticks: &Vec<f64>, min: f64, max: f64, face_width: u32) ->
         .collect()
 }
 
+/// An x-axis label for the text output renderer
+#[derive(Debug)]
+struct XAxisLabel {
+    text: String,
+    offset: u32,
+}
+
+impl XAxisLabel {
+    fn len(&self) -> usize {
+        self.text.len()
+    }
+
+    /// The number of cells the label will actually use
+    /// We want this to always be an odd number
+    fn footprint(&self) -> usize {
+        if self.len() % 2 == 0 {
+            self.len() + 1
+        } else {
+            self.len()
+        }
+    }
+
+    /// The offset, relative to the zero-point of the axis where the label should start to be drawn
+    fn start_offset(&self) -> i32 {
+        self.offset as i32 - self.footprint() as i32 / 2
+    }
+}
+
+#[test]
+fn test_x_axis_label() {
+    let l = XAxisLabel {
+        text: "3".to_string(),
+        offset: 2,
+    };
+    assert_eq!(l.len(), 1);
+    assert!(l.footprint() % 2 != 0);
+    assert_eq!(l.start_offset(), 2);
+
+    let l = XAxisLabel {
+        text: "34".to_string(),
+        offset: 2,
+    };
+    assert_eq!(l.len(), 2);
+    assert!(l.footprint() % 2 != 0);
+    assert_eq!(l.start_offset(), 1);
+
+    let l = XAxisLabel {
+        text: "345".to_string(),
+        offset: 2,
+    };
+    assert_eq!(l.len(), 3);
+    assert!(l.footprint() % 2 != 0);
+    assert_eq!(l.start_offset(), 1);
+
+    let l = XAxisLabel {
+        text: "3454".to_string(),
+        offset: 1,
+    };
+    assert_eq!(l.len(), 4);
+    assert!(l.footprint() % 2 != 0);
+    assert_eq!(l.start_offset(), -1);
+}
+
+fn create_x_axis_labels(tick_map: &HashMap<u32, f64>) -> Vec<XAxisLabel> {
+    let mut ls = Vec::from_iter(tick_map.iter().map(|(&offset, &tick)| {
+        XAxisLabel {
+            text: tick.to_string(),
+            offset: offset,
+        }
+    }));
+    ls.sort_by_key(|l| l.offset);
+    ls
+}
+
 pub fn draw_histogram(h: histogram::Histogram) {
     // The face is the actual area of the graph with data on it, excluding axes and labels
     let face_width = h.num_bins() * 3;
@@ -266,6 +341,49 @@ pub fn draw_histogram(h: histogram::Histogram) {
         .expect("ERROR: There are no y-axis ticks");
 
     let y_axis_strings = distribute_y_ticks(y_ticks, largest_bin_count as f64, face_height);
+
+    let min = *h.bin_bounds.first().expect("ERROR: There are no ticks for the x-axis");
+    let max = *h.bin_bounds.last().expect("ERROR: There are no ticks for the x-axis");
+    let x_tick_step = calculate_tick_step_for_range(min, max, max_x_ticks);
+    let x_ticks = generate_ticks(min, max, x_tick_step);
+    let tick_map = tick_offset_map(&x_ticks, min, max, face_width as u32);
+    let mut keys = Vec::from_iter(tick_map.keys());
+    keys.sort();
+
+    let mut tick_marks = "".to_string();
+    for cell in 0..face_width + 1 {
+        let cell = cell as u32;
+        let ch = if tick_map.get(&cell).is_some() {
+            '|'
+        } else {
+            ' '
+        };
+        tick_marks.push(ch);
+    }
+
+    let x_labels = create_x_axis_labels(&tick_map);
+    let start_offset = x_labels.iter()
+        .map(|label| label.start_offset())
+        .min()
+        .expect("ERROR: Could not compute start offset of x-axis");
+
+    // This string will be printed, starting at start_offset relative to the x-axis zero cell
+    let mut tick_label_string = "".to_string();
+
+    for label in (&x_labels).iter() {
+        let spaces_to_append = label.start_offset() - start_offset - tick_label_string.len() as i32;
+        if spaces_to_append.is_positive() {
+            for i in 0..spaces_to_append {
+                tick_label_string.push(' ');
+            }
+        } else {
+            for i in 0..spaces_to_append.wrapping_neg() {
+                tick_label_string.pop();
+            }
+        }
+        let formatted_label = format!("{: ^footprint$}", label.text, footprint = label.footprint());
+        tick_label_string.push_str(&formatted_label);
+    }
 
     for line in 0..face_height {
         let axis_label = y_axis_strings[(face_height - line) as usize].to_string();
@@ -294,27 +412,15 @@ pub fn draw_histogram(h: histogram::Histogram) {
              label_width = longest_y_label_width,
              plot_width = face_width);
 
-    let min = *h.bin_bounds.first().expect("ERROR: There are no ticks for the x-axis");
-    let max = *h.bin_bounds.last().expect("ERROR: There are no ticks for the x-axis");
-    let x_tick_step = calculate_tick_step_for_range(min, max, max_x_ticks);
-    let x_ticks = generate_ticks(min, max, x_tick_step);
-    let tick_map = tick_offset_map(&x_ticks, min, max, face_width as u32);
-    let mut keys = Vec::from_iter(tick_map.keys());
-    keys.sort();
-
-    let mut tick_marks = "".to_string();
-    for cell in 0..face_width + 1 {
-        let cell = cell as u32;
-        let ch = if tick_map.get(&cell).is_some() {
-            '|'
-        } else {
-            ' '
-        };
-        tick_marks.push(ch);
-    }
     println!("{:>label_width$} {:-<plot_width$}",
              "",
              tick_marks,
              label_width = longest_y_label_width,
              plot_width = face_width + 1);
+
+    println!("{:>label_width$} {: <plot_width$}",
+             "",
+             tick_label_string,
+             label_width = (longest_y_label_width as i32 + start_offset) as usize,
+             plot_width = (face_width as i32 + 1 + start_offset) as usize);
 }
