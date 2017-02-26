b1d04e9e83cbb2c0ca9c199c7d381d46326df1b9
Matt Williams <matt@milliams.com>
2017-02-26 19:54:25 +0000

    Rearrange rendering code to separate data construction and printing

--- src/render.rs
+++ src/render.rs
@@ -2,6 +2,7 @@
 
 use std::collections::HashMap;
 use std::iter::FromIterator;
+use std;
 
 use histogram;
 use axis;
@@ -85,8 +86,32 @@ pub fn draw_histogram(h: histogram::Histogram) {
         .max()
         .expect("ERROR: There are no y-axis ticks");
 
+    // Generate a list of strings to label the y-axis
+    let y_label_strings = Vec::from_iter((0..face_height + 1)
+        .map(|line| match y_tick_map.get(&line) {
+            Some(v) => v.to_string(),
+            None => "".to_string(),
+        }));
+
+    // Generate a list of strings to tick the y-axis
+    let y_tick_strings = Vec::from_iter((0..face_height + 1)
+        .map(|line| match y_tick_map.get(&line) {
+            Some(_) => "-".to_string(),
+            None => " ".to_string(),
+        }));
+
+    // Generate a list of strings to be the y-axis line itself
+    let mut y_axis_line_strings = vec![];
+    {
+        let axis_corner = vec!["+".to_string()];
+        let axis_lines = vec!["|".to_string(); face_height as usize];
+        y_axis_line_strings.extend(axis_corner);
+        y_axis_line_strings.extend(axis_lines);
+    }
+    let y_axis_line_strings = y_axis_line_strings;
+
     // Create a string which will be printed to give the x-axis tick marks
-    let mut x_tick_marks = "".to_string();
+    let mut x_axis_tick_string = "".to_string();
     for cell in 0..face_width + 1 {
         let cell = cell as u32;
         let ch = if x_tick_map.get(&cell).is_some() {
@@ -94,7 +119,7 @@ pub fn draw_histogram(h: histogram::Histogram) {
         } else {
             ' '
         };
-        x_tick_marks.push(ch);
+        x_axis_tick_string.push(ch);
     }
 
     // Create a string which will be printed to give the x-axis labels
@@ -107,7 +132,8 @@ pub fn draw_histogram(h: histogram::Histogram) {
     // This string will be printed, starting at start_offset relative to the x-axis zero cell
     let mut x_axis_label_string = "".to_string();
     for label in (&x_labels).iter() {
-        let spaces_to_append = label.start_offset() - start_offset - x_axis_label_string.len() as i32;
+        let spaces_to_append = label.start_offset() - start_offset -
+                               x_axis_label_string.len() as i32;
         if spaces_to_append.is_positive() {
             for _ in 0..spaces_to_append {
                 x_axis_label_string.push(' ');
@@ -121,51 +147,74 @@ pub fn draw_histogram(h: histogram::Histogram) {
         x_axis_label_string.push_str(&formatted_label);
     }
 
-    // Go, line-by-line printing the y-axis and the face
+    // Generate a list of strings to be the y-axis line itself
+    let mut x_axis_line_string = String::new();
+    {
+        let axis_lines = vec!['-'; face_width as usize];
+        x_axis_line_string.push('+');
+        x_axis_line_string.extend(axis_lines);
+    }
+    let x_axis_line_string = x_axis_line_string;
+
+    let mut face_strings: Vec<String> = vec![];
+
     for line in 0..face_height {
-        let cell_position = face_height - line;
-        let axis_label = match y_tick_map.get(&cell_position) {
-            Some(v) => v.to_string(),
-            None => "".to_string(),
-        };
         let mut cols = String::new();
         for &bin_count in h.bin_counts.iter() {
             // between 0..1 how full the bin is compared to largest
-            let bin_height_characters = value_to_axis_cell_offset(bin_count as f64, &y_axis, face_height);
-            if bin_height_characters == cell_position {
-                cols.push_str("---");
-            } else if bin_height_characters > cell_position {
-                cols.push_str("| |");
+            let bin_height_characters =
+                value_to_axis_cell_offset(bin_count as f64, &y_axis, face_height);
+            if bin_height_characters > line {
+                cols.push_str("###");
             } else {
                 cols.push_str("   ");
             }
         }
-
-        println!("{:>label_width$} |{}",
-                 axis_label,
-                 cols,
-                 label_width = longest_y_label_width);
+        face_strings.push(cols);
     }
 
+    let left_gutter_width = std::cmp::max(longest_y_label_width as i32 + 1,
+                                          (start_offset as i32).wrapping_neg()) as
+                            usize;
+
+    // Go, line-by-line printing the y-axis and the face
+    for line in 0..face_height {
+        let cell_position = face_height - line;
+        let ref axis_label = y_label_strings[*&cell_position as usize];
+        let ref axis_tick = y_tick_strings[*&cell_position as usize];
+        let ref axis_line = y_axis_line_strings[*&cell_position as usize];
+        let mut axis_label_and_tick = String::new();
+        axis_label_and_tick.push_str(axis_label);
+        axis_label_and_tick.push_str(axis_tick);
+        let ref face_line = face_strings[(*&cell_position as usize) - 1];
+        println!("{:>left_gutter_width$}{}{}",
+                 axis_label_and_tick,
+                 axis_line,
+                 face_line,
+                 left_gutter_width = left_gutter_width);
+    }
+
+    // Avoid duplicating the '+'. TODO combine them cleverly
+    let reduced_x_axis_line = x_axis_line_string[1..].to_string();
     // Print the x-axis line and the zero-point of the y-axis
-    println!("{:>label_width$} +{:-<plot_width$}",
-             "0",
-             "",
-             label_width = longest_y_label_width,
-             plot_width = face_width);
+    println!("{:>left_gutter_width$}{}{}",
+             y_label_strings[0],
+             y_axis_line_strings[0],
+             reduced_x_axis_line,
+             left_gutter_width = left_gutter_width);
 
     // Print the x-axis ticks
-    println!("{:>label_width$} {:-<plot_width$}",
+    println!("{:>left_gutter_width$}{:-<plot_width$}",
              "",
-             x_tick_marks,
-             label_width = longest_y_label_width,
+             x_axis_tick_string,
+             left_gutter_width = left_gutter_width,
              plot_width = face_width + 1);
 
     // Print the x-axis labels
-    println!("{:>label_width$} {: <plot_width$}",
+    println!("{:>start_offset$}{: <plot_width$}",
              "",
              x_axis_label_string,
-             label_width = (longest_y_label_width as i32 + start_offset) as usize,
+             start_offset = (left_gutter_width as i32 + start_offset) as usize,
              plot_width = (face_width as i32 + 1 + start_offset) as usize);
 }
 
