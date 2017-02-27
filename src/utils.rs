ba6f773ad8ef39124227509209ff66c5dfff07ec
Matt Williams <matt@milliams.com>
2017-02-27 20:45:53 +0000

    Add tests for `pairwise()`

--- src/utils.rs
+++ src/utils.rs
@@ -6,3 +6,24 @@ pub fn pairwise<T>(container: &[T])
     let second = container.iter().skip(1);
     first.zip(second)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pairwise() {
+        let a = [1, 2, 3, 4, 5];
+        assert_eq!(pairwise(&a).nth(0).unwrap(), (&1, &2));
+        assert_eq!(pairwise(&a).last().unwrap(), (&4, &5));
+        assert_eq!(pairwise(&a).len(), a.len() - 1);
+
+        let a = [1, 2];
+        assert_eq!(pairwise(&a).nth(0).unwrap(), (&1, &2));
+        assert_eq!(pairwise(&a).last().unwrap(), (&1, &2));
+        assert_eq!(pairwise(&a).len(), a.len() - 1);
+
+        let a = [1];
+        assert!(pairwise(&a).nth(0).is_none());
+    }
+}
